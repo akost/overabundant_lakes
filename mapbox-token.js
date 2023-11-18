5e2ef624f6f1b4b1fe182042c464a09564ef2779
Andrei K <kost@kost.ru>
2023-11-17 20:00:54 -0800

    Replace token

--- mapbox-token.js
+++ mapbox-token.js
@@ -1,1 +1,1 @@
-const MAPBOX_TOKEN="pk.eyJ1IjoicG9vLXBvbyIsImEiOiJjbG9qa2h6MWIwYnA2MmtxbzJqMTAwdnFmIn0.ZnxzxjO1u7qRKcvq-sflyA";
+const MAPBOX_TOKEN="pk.eyJ1IjoicG9vLXBvbyIsImEiOiJjbG9qa20waDIweThuMnJwcWVzcmZ1bHBlIn0.w9-rjJodOTVlSFGS8V417A";
