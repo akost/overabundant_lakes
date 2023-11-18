717473503294e2af542600950954276bee051c0d
Andrei K <kost@kost.ru>
2023-11-17 20:13:51 -0800

    Use my default token

--- mapbox-token.js
+++ mapbox-token.js
@@ -1,1 +1,1 @@
-const MAPBOX_TOKEN="pk.eyJ1IjoicG9vLXBvbyIsImEiOiJjbG9qa20waDIweThuMnJwcWVzcmZ1bHBlIn0.w9-rjJodOTVlSFGS8V417A";
+const MAPBOX_TOKEN="pk.eyJ1IjoicG9vLXBvbyIsImEiOiJjbG9qa2h6MWIwYnA2MmtxbzJqMTAwdnFmIn0.ZnxzxjO1u7qRKcvq-sflyA";
